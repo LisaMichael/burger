1d9fd0839f7b209a605a49fe90a7d095df8f54e6
Lisa <lisa.michael@hotmail.com>
2019-12-27 23:53:55 -0500

    updated routes.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,12 @@
+
+// references to express and router 
 var express = require('express'); 
 var router = express.Router();
 
+//base route
 router.get('/', function(req, res){
 res.render('index');
 
 })
+
+module.exports = router;
